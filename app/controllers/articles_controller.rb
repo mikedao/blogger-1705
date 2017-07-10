14017ffb6bff6a4caef210a7f96a6feff0ccfea1
Michael Dao <michael.dao@gmail.com>
2017-07-10 13:16:57 -0600

    I2 complete

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :show, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
